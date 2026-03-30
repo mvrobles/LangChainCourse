3aaa8d3967936aa13303a195a78aa1cebde10b52
Melissa Robles <melissa.robles07@gmail.com>
2026-03-30 10:58:04 -0500

    tavily search

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
+#from tavily import TavilyClient
 
-tavily = TavilyClient()
+#tavily = TavilyClient()
 
 ###########################################################
 # Tool: Function that an LLM can excecute
@@ -16,20 +17,20 @@ tavily = TavilyClient()
 # El docstring lo conoce el LLM para entender cuándo usarlo
 # Type hinting y nombre de variables también es importante
 
-def search(query: str) -> str:
-    """Search the web for the given query and return the results.
+# def search(query: str) -> str:
+#     """Search the web for the given query and return the results.
 
-    Args:
-        query: The search query string to look up.
+#     Args:
+#         query: The search query string to look up.
 
-    Returns:
-        A string containing the search results.
-    """
-    print(f'Searching {query}')
-    return tavily.search(query=query)
+#     Returns:
+#         A string containing the search results.
+#     """
+#     print(f'Searching {query}')
+#     return tavily.search(query=query)
 
 llm = ChatOpenAI()
-tools = [search]
+tools = [TavilySearch()]
 agent = create_agent(model = llm, tools = tools)
 
 def main():
