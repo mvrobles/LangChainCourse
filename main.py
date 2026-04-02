381641da83f4d07cd224c65ca26d4cbe1c7cf17d
Melissa Robles <melissa.robles07@gmail.com>
2026-04-02 17:13:35 -0500

    theory

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+##### Theory 
+
+# Los modelos SOTA manejan una forma de poner sus respuestas en un formato que queramos (específico)
+# Por default Langchain usa esto - la forma nativa de los modelos
+# Si no lo soporta usa Tool calling strategy
+# --- PydanticModel, Dataclass, Json, etc.
+
 from dotenv import load_dotenv
 
 load_dotenv()
+
+from typing import List
+from pydantic import BaseModel, Field # Structured datascheme. Automatic datatype validation
 
 from langchain.agents import create_agent
 from langchain.tools import tool
@@ -29,14 +39,24 @@ from langchain_tavily import TavilySearch
 #     print(f'Searching {query}')
 #     return tavily.search(query=query)
 
-llm = ChatOpenAI()
+class Source(BaseModel):
+    """Schema for a source used by the agente"""
+    url: str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+    answer: str = Field(description="The agent's answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used to generate the answer") # If there's no source list it takes empty list
+
+llm = ChatOpenAI(model = 'gpt-5')
 tools = [TavilySearch()]
-agent = create_agent(model = llm, tools = tools)
+agent = create_agent(model = llm, tools = tools, response_format=AgentResponse) 
 
 def main():
     print("Hello!")
     result = agent.invoke({
-        "messages": HumanMessage(content="Search for 3 job postings in AI/Data Science in Bogotá Colombia or with possibility of working from Bogotá Colombia on Linkedin. Give me the details.")})
+        "messages": HumanMessage(
+            content="Search for 3 job postings in AI/Data Science in Bogotá Colombia or with possibility of working from Bogotá Colombia on Linkedin. Give me the details.")})
     print(result)
 
 if __name__ == '__main__':
